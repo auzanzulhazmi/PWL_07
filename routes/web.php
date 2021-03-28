ad3a2c280c92fb77d57dcb4049b72c8abe9ad8c9
auzanzulhazmi <zulhazmiauzan@gmail.com>
2021-03-28 09:58:58 +0700

    tambah kode pada route web

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\MahasiswaController;
+use Illuminate\Http\Request;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,3 +18,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::resource('mahasiswa', MahasiswaController::class);
